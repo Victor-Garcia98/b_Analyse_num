cfd603cfca03a5a7812799c856ad7cbc448e7759
Antonin <antonin.chiarotto.1@ens.etsmtl.ca>
2025-02-27 14:38:16 -0500

    ajout d'une fonction intégration qui permet de calculer une intégration entre 2 bornes d'un polynome de degré 3

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+
+def integration(p1,p2,p3,p4,a,b) :
+    return (p1 * b + (p2 / 2) * b ** 2 + (p3 / 3) * a ** 3 + (p4 / 4) * a ** 4) - (p1 * a + (p2 / 2) * a ** 2 + (p3 / 3) * a ** 3 + (p4 / 4) * a ** 4)
+
+
+resultat = integration(1, 2, 3, 4, 0, 1)
+print(resultat)
